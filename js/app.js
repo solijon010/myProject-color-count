9f6c10a3144ef3c8216cd26ab1f4ed8b0fd05cc1
Solijon <solijonikromov001@gamil.com>
2025-10-28 11:38:06 +0500

    site 2-qism

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// const userName = window.prompt("ismingizni yozing!");
+
+// innerHTML
+// const title = document.querySelector("h1");
+// // title.innerHTML += ` <span style="color=orange">${userName}!</span>`;
+
+// const sarlovha = document.createElement("h2");
+
+// sarlovha.textContent = "Bu yangi yaratilgan sarlovha";
+// document.body.append(sarlovha);
+
+// Info: :localstorage
+// Primative
+// localStorage.setItem("name", "Solijon");
+// console.log(localStorage.getItem("name"));
+
+// JSON=> JavaScript Object Notation
+
+// non-primative
+// const user = {
+//   name: "Solijon",
+//   age: 22,
+//   isMarried: false,
+// };
+
+// // localStorage.setItem("userInfo", JSON.stringify(user));
+
+// const newUser = JSON.parse(localStorage.getItem("userInfo"));
+
+// console.log(newUser.age);
+
+// IDea: NEw Project boshlandi
+
+const bodyEl = document.body;
+const formEl = document.querySelector(".form");
+
+formEl.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(formEl);
+  const color = formData.get("color");
+  bodyEl.style.backgroundColor = color;
+  formEl.reset();
+});
