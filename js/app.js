21b269b2e07e9d12945236d1d4e89da2266b9db8
Solijon <solijonikromov001@gamil.com>
2025-10-28 13:35:24 +0500

    site 3-qism

--- js/app.js
+++ js/app.js
@@ -33,11 +33,27 @@
 
 const bodyEl = document.body;
 const formEl = document.querySelector(".form");
+const wrapperEl = document.querySelector(".wrapper");
+
+const colors = [];
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   const formData = new FormData(formEl);
-  const color = formData.get("color");
+  const color = formData.get("color").trim();
   bodyEl.style.backgroundColor = color;
+  if (
+    bodyEl.style.backgroundColor === color &&
+    colors[colors.length - 1] != color
+  ) {
+    let squareEl = document.createElement("span");
+    squareEl.classList.add("square");
+    squareEl.style.backgroundColor = color;
+    wrapperEl.append(squareEl);
+    colors.push(color);
+    localStorage.setItem('colors',JSON.stringify(colors))
+  }
+  console.log(colors);
+
   formEl.reset();
 });
